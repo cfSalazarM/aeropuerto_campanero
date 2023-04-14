c28cd637db7f0b65fb579c1a8e6fc8e6902ca638
cf_salazar <felipesalazar@udenar.edu.co>
2023-04-13 22:01:39 -0500

    Agregando funcionaldad de busqueda de vuelo

--- src/controllers/availabilityFlight.controller.js
+++ src/controllers/availabilityFlight.controller.js
@@ -17,18 +17,29 @@ const AvailabilityFlight = {
 
             if (result) {
                 const t = moment(`${result.date} ${result.time}`);
-                console.log(t.get('month'));
+                t.add(1, 'hours');
+                console.log(t.format('YYYY-MM-DD HH:mm'));
                 divElement.innerHTML =
                     `<div class="m-5 shadow p-3 mb-5 bg-body-tertiary rounded">
                         <h5><i class="fa-solid fa-plane px-2"></i>Salida de ${availability.cityOrigin} a ${availability.cityDestiny} - ${availability.date}</h5>
                         <div class="row mt-5">
                             <div class="card w-75 m-5">
-                                <div class="card-body d-flex justify-content-between">
-                                    <h5>${result.time}</h5>
-                                    <hr class="w-25 justify-self-start"/>
-                                    <i class="fa-solid fa-plane-departure"></i>
-                                    <hr class="w-25 justify-self-start"/>    
-                                    <h5>${result.time}</h5>
+                                <div class="card-body ">
+                                    <div class="d-flex justify-content-between">
+                                        <h5>${result.time}</h5>
+                                        <hr class="w-25 justify-self-start"/>
+                                        <i class="fa-solid fa-plane-departure"></i>
+                                        <hr class="w-25 justify-self-start"/>    
+                                        <h5>${t.format('HH:mm')}</h5>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            <p>${result.cityOrigin}</p>
+                                        </div>
+                                        <div>
+                                            <p>${result.cityDestiny}</p>
+                                        </div>
+                                    </div>
                                     
                                 </div>
                             </div>
@@ -40,8 +51,7 @@ const AvailabilityFlight = {
             else {
                 divElement.innerHTML =
                 `<div class="m-5 shadow p-3 mb-5 bg-body-tertiary rounded">
-
-                Larger shadow
+                    <h5>Lo sentimos no se econtraron resultados para tu busqueda</h5>
                 </div>`
             }
             console.log(result);
